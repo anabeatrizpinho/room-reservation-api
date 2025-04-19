0add2ab926290a143a098653126463d013d3fc62
Ana Beatriz <biapinho@live.com>
2025-04-19 04:23:09 -0300

    refactor(core): update app controller and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { MeetingRoomsModule } from './meeting_rooms/meeting_rooms.module';
+import { ReservationsModule } from './reservations/reservations.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [UsersModule, ReservationsModule, AuthModule, MeetingRoomsModule],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
